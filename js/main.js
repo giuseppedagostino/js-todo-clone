b140a4a4a3872da9189cc19aa7545fa2f7782508
Giuseppe <giuseppe.dagostino.d@gmail.com>
2020-11-30 15:57:37 +0100

    Stampo a schermo la to-do list
    - Creazione array contenente la lista
    - Scorrimento array con ciclo for per stampare tutti i suoi elementi

--- js/main.js
+++ js/main.js
@@ -4,5 +4,27 @@
 $(document).ready(
   function() {
 
+    // Creo la todo list
+    var list = [
+      "Seguire la lezione di Gianluca",
+      "Pranzare",
+      "Fare l'esercizio giornaliero",
+      "Aprire un hotwell",
+    ];
+    console.log(list);
+
+    // Dico ad Handlebars cosa usare
+    var source = $("#entry-template").html();
+    var template = Handlebars.compile(source);
+
+    // Scorro l'array lista e genero l'html
+    for (var i = 0; i < list.length; i++) {
+      var context = {
+        text: list[i]
+      };
+      var html = template(context);
+      $("#todo").append(html);
+    }
+
   }
 );
