c01664ec0d6e06be92cc74372fa25eb81226a9ad
Giuseppe <giuseppe.dagostino.d@gmail.com>
2020-11-30 13:15:32 +0100

    Avvio progetto

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// Esercizio: creare una to-do list, come quella fatta insieme stamattina, utilizzando Handlebars come templating engine.
+// Consiglio: fate uno step alla volta, prima caricate Handlebars da CDN in pagina, poi provate a seguire i primi step della sezione "Usage", modificando le proprietà dell'oggetto context.
+
+$(document).ready(
+  function() {
+
+  }
+);
